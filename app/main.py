9c6feca9af5882e008ef4bd25c5292571f0a9cf9
timovlad <61474934+timovlad@users.noreply.github.com>
2024-11-13 17:17:24 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,29 @@
+import math
+from typing import Tuple, List, Dict, Any
+
+
 class Person:
-    # write your code here
-    pass
+    people: Dict[str, 'Person'] = {}
+
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+        self.wife = None
+        self.husband = None
+        Person.people[name] = self
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people: List[Dict[str, Any]]) -> List[Person]:
+    # Create initial list of Person instances
+    person_list = [Person(person['name'], person['age']) for person in people]
+
+    # Assign wife/husband attributes where applicable
+    for person in people:
+        person_instance = Person.people[person['name']]
+        if 'wife' in person and person['wife']:
+            person_instance.wife = Person.people[person['wife']]
+        elif 'husband' in person and person['husband']:
+            person_instance.husband = Person.people[person['husband']]
+
+    return person_list
+    
